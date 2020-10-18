9455773690b09da076403119b6a2c85a18aeb625
Ryan Helgerson <ryan.helgerson@gmail.com>
2020-10-18 15:52:28 -0500

    Schema / custom components built

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -1,4 +1,5 @@
 import { MdLocalPizza as icon } from 'react-icons/md';
+import PriceInput from '../components/PriceInput';
 
 export default {
   // Computer name
@@ -38,6 +39,34 @@ export default {
       description: 'Price of the pizza in cents',
       validation: (Rule) => Rule.min(1000).max(5000),
       // TODO: Add custom component
+      inputComponent: PriceInput,
+    },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
     },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      // 1. Filter out undefined toppings
+      const tops = Object.values(toppings).filter(Boolean);
+
+      // 2. Return the preview Object for the pizza
+      return {
+        title,
+        media,
+        subtitle: tops.join(', '),
+      };
+    },
+  },
 };
